3c1307a75b6b9f76509d1b314f151feff1ea321d
Sekib Omazic <sekib.omazic@comsysto.com>
2013-04-14 00:38:08 +0200

    configuration changed

--- app/js/app.js
+++ app/js/app.js
@@ -2,4 +2,4 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('chartjsApp', ['chartjsApp.filters', 'chartjsApp.services', 'chartjsApp.directives']);
+angular.module('chartjsApp', ['chartjsApp.filters', 'chartjsApp.services', 'chartjsApp.directives', 'chartjsApp.controllers']);
